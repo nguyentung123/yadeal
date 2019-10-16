dd583db8927fb95c4dc56f909079caa5e571638f
Nhat Tran <tmnhat1993@gmail.com>
2019-10-16 07:52:59 +0700

    Remove Unused File & Optimize Header Part

--- header.php
+++ header.php
@@ -1,16 +1,3 @@
-<?php
-/**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package Yadea_Theme
- */
-
-?>
-
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
@@ -47,8 +34,8 @@
 
             <!-- Header Menu Wrapper -->
             <div class="col-lg-10 col-6 header__menu">
-                <!--Get Menu-->
 
+                <!-- Header Main Menu -->
                 <ul class="nav justify-content-end mb-hide">
                     <li class="nav-item <?php if(is_page('products')) echo 'active'; ?>" id="sub-menu-trigger">
                         <a class="nav-link" href="<?php echo site_url('/products'); ?>">Sản phẩm</a>
@@ -63,11 +50,15 @@
                         <a class="nav-link" href="<?php echo site_url('/support') ?>">Hỗ trợ</a>
                     </li>
                 </ul>
+                <!-- Header Main Menu -->
 
+                <!-- Header Mobile Menu Toggle -->
                 <div class="header__menu__btn mb-show-bl" id="mobile-menu-ctrl">
                     <i class="fa fa-bars" aria-hidden="true" id="open-menu-btn"></i>
                     <i class="fa fa-times" aria-hidden="true" id="close-menu-btn"></i>
                 </div>
+                <!-- Header Mobile Menu Toggle -->
+
             </div><!-- Header Menu Wrapper -->
 
 
@@ -77,73 +68,76 @@
 
     <!-- Fixed Position Sub Menu -->
     <?php
-
-    $args = array(
-        "post_type" => 'product',
-        "post_status" => 'publish',
-        'posts_per_page'=>-1
+    $productHeaderQueryOptions = array(
+        'post_type'       => 'product',
+        'post_status'     => 'publish',
+        'posts_per_page'  => 6,
+        'meta_query'      => array(
+            array(
+                'key'     => 'show_in_menu_header',
+                'value'   => 'show',
+                'compare' => 'LIKE'
+            )
+        )
     );
-    $query=new WP_Query($args);
-    if($query->have_posts()):
-        ?>
+    $productHeaderQuery = new WP_Query($productHeaderQueryOptions);
+
+    if($productHeaderQuery->have_posts()): ?>
+        <!-- Product Listing Sub Menu -->
         <div class="container sub-menu mb-hide" id="page-sub-menu">
 
             <!-- Row - Product Wrapper -->
             <div class="sub-menu__wrapper row product-list-layout">
-                <?php $number_show=0; while ($query->have_posts()):$query->the_post(); if($number_show>=6){break;}
-                    $show = get_field('show_in_menu_header');
-                    if($show[0]=='show'){
-                        $number_show++;
-                        ?>
 
-                        <!-- Product Item -->
-                        <div class="col-md-4 col-6 product-item">
+                <?php while ($productHeaderQuery->have_posts()): ?>
+                    <?php $productHeaderQuery->the_post(); ?>
 
-                            <!-- Product Detail Wrapper -->
-                            <div class="product-detail">
+                    <!-- Product Item -->
+                    <div class="col-md-4 col-6 product-item">
 
-                                <!-- Color Variation -->
-                                <?php $list_color_pro = get_field('product_colors'); ?>
-                                <ul class="color-variations horizontal-list text-right">
-                                    <?php foreach ($list_color_pro as $color_name): ?>
-                                        <li class="color-variations__item <?php echo $color_name;?> "></li>
-                                    <?php endforeach; ?>
-                                </ul>
-                                <!-- Color Variation -->
+                        <!-- Product Detail Wrapper -->
+                        <div class="product-detail">
 
-                                <!-- Product Image -->
-                                <div class="product-image">
-                                    <?php
-                                    $url_img = has_post_thumbnail($post->ID) ? get_the_post_thumbnail_url($post->ID): get_theme_file_uri('assets/img/product1.png');
-                                    ?>
-                                    <a href="<?php echo site_url('/products'); ?>">
-                                        <img src="<?php echo $url_img;?>" class="img-fluid">
-                                    </a>
-                                </div>
-                                <!-- Product Image -->
+                            <!-- Color Variation -->
+                            <?php $list_color_pro = get_field('product_colors'); ?>
+                            <ul class="color-variations horizontal-list text-right">
+                                <?php foreach ($list_color_pro as $color_name): ?>
+                                    <li class="color-variations__item <?php echo $color_name;?> "></li>
+                                <?php endforeach; ?>
+                            </ul>
+                            <!-- Color Variation -->
 
-                                <!-- Product Context -->
-                                <div class="product-context text-center">
-                                    <h4 class="product-name page-desc">
-                                        <a href="<?php echo site_url('/products'); ?>">
-                                            <?php echo get_field('product_name')?get_field('product_name'):get_the_title();?>
-                                        </a>
-                                    </h4>
-                                </div>
-                                <!-- Product Context -->
-
+                            <!-- Product Image -->
+                            <div class="product-image">
+                                <?php
+                                $url_img = has_post_thumbnail($post->ID) ? get_the_post_thumbnail_url($post->ID): get_theme_file_uri('assets/img/product1.png');
+                                ?>
+                                <a href="<?php echo site_url('/products'); ?>">
+                                    <img src="<?php echo $url_img;?>" class="img-fluid">
+                                </a>
                             </div>
-                        </div><!-- Product Item -->
+                            <!-- Product Image -->
 
-                        <?php
-                    }
-                    ?>
-                <?php endwhile;?>
+                            <!-- Product Context -->
+                            <div class="product-context text-center">
+                                <h4 class="product-name page-desc">
+                                    <a href="<?php echo site_url('/products'); ?>">
+                                        <?php echo get_field('product_name')?get_field('product_name'):get_the_title();?>
+                                    </a>
+                                </h4>
+                            </div>
+                            <!-- Product Context -->
+
+                        </div>
+                    </div><!-- Product Item -->
+
+                <?php endwhile; ?>
             </div>
         </div>
-    <?php endif; wp_reset_postdata();?>
 
-    <!-- Fixed Position Sub Menu -->
+    <?php endif; ?>
+
+
     <!-- Fixed Position Sub Menu -->
     <div class="container sub-menu" id="about-us-sub-menu">
         <!-- Row - About Wrapper -->
@@ -153,9 +147,9 @@
 
                 <!-- About Detail Wrapper -->
                 <div class="sub-menu__wrapper__item__detail">
-                    <a href="<?php echo site_url('/about') ?>">
+                    <a href="<?php echo site_url('/about'); ?>">
                         <div class="about-us__thumbnail">
-                            <img class="img-fluid" src="<?php echo get_theme_file_uri('assets/img/about-thumbnail.jpg');?>">
+                            <img class="img-fluid" src="<?php echo get_theme_file_uri('assets/img/about-thumbnail.jpg'); ?>">
                         </div>
                         <p class="sub-title">Giới thiệu Yadea</p>
                         <p class="txt-gray desc">Công ty TNHH Xe Máy Điện Thông Minh YADEA</p>
@@ -168,9 +162,9 @@
             <div class="col-md-4 col-6 sub-menu__wrapper__item">
                 <!-- About Detail Wrapper -->
                 <div class="sub-menu__wrapper__item__detail">
-                    <a href="<?php echo site_url('/course') ?>">
+                    <a href="<?php echo site_url('/course'); ?>">
                         <div class="about-us__thumbnail">
-                            <img class="img-fluid" src="<?php echo get_theme_file_uri('assets/img/history-thumbnail.jpg');?>">
+                            <img class="img-fluid" src="<?php echo get_theme_file_uri('assets/img/history-thumbnail.jpg'); ?>">
                         </div>
                         <p class="sub-title">Quá trình phát triển</p>
                         <p class="txt-gray desc">Lịch sử hình thành - quá trình phát triển của YADEA</p>
@@ -184,9 +178,9 @@
 
                 <!-- About Detail Wrapper -->
                 <div class="sub-menu__wrapper__item__detail">
-                    <a href="<?php echo site_url('/supplier') ?>">
+                    <a href="<?php echo site_url('/supplier'); ?>">
                         <div class="about-us__thumbnail">
-                            <img class="img-fluid" src="<?php echo get_theme_file_uri('assets/img/supplier-thumbnail.jpg');?>">
+                            <img class="img-fluid" src="<?php echo get_theme_file_uri('assets/img/supplier-thumbnail.jpg'); ?>">
                         </div>
                         <p class="sub-title">Đối tác hàng đầu</p>
                         <p class="txt-gray desc">Đối tác hàng đầu của chúng tôi</p>
@@ -199,60 +193,62 @@
 
     <!-- Mobile Drop Down Menu -->
     <div class="mobile-menu" id="mobile-menu">
+
+        <!-- Overlay -->
         <div class="mobile-menu__overlay"></div>
+
+        <!-- Main Mobile Dropdown Menu -->
         <ul class="nav light-gray-bg">
+
+            <!-- Product Listing -->
             <li class="nav-item with-sub-menu <?php if(is_page('/shops')) echo 'active'; ?>">
                 <a class="nav-link" href="<?php echo site_url('/products'); ?>">Sản phẩm</a>
-                <i class="fa fa-plus nav-trigger active" aria-hidden="true"></i>
-                <?php
 
-                $args = array(
-                    "post_type" => 'product',
-                    "post_status" => 'publish',
-                    'posts_per_page'=>-1
-                );
-                $query=new WP_Query($args);
-                if($query->have_posts()):
-                    ?>
+                <?php if($productHeaderQuery->have_posts()): ?>
+
+                    <i class="fa fa-plus nav-trigger active" aria-hidden="true"></i>
                     <ul class="nav-item__sub-menu">
-                        <?php $number_show=0; while ($query->have_posts()):$query->the_post(); if($number_show>=6){break;}
-                            $show = get_field('show_in_menu_header');
-                            if($show[0]=='show'){
-                                $number_show++;
-                                ?>
-                                <!-- Product Item -->
-                                <li class="product-item">
-                                    <a href="<?php echo site_url('/products'); ?>">
-                                        <!-- Product image -->
-                                        <?php
-                                        $url_img = get_the_post_thumbnail_url($post->ID)? get_the_post_thumbnail_url($post->ID):get_theme_file_uri('assets/img/product1.png');
-                                        ?>
-                                        <img class="product-image" src="<?php echo $url_img; ?>">
-                                        <!-- Color Variation -->
-                                        <?php
-                                        $list_color_pro = get_field('product_colors');
-                                        if($list_color_pro):
-                                            ?>
-                                            <ul class="color-variations horizontal-list text-right pr-15">
-                                                <?php foreach( $list_color_pro as $color_pro): ?>
-                                                    <li class="color-variations__item <?php echo $color_pro; ?>"></li>
-                                                <?php endforeach; ?>
-                                            </ul><!-- Color Variation -->
-                                        <?php endif;?>
+                        <?php while ($query->have_posts()): ?>
+                            <?php $query->the_post(); ?>
 
-                                        <!-- Product Name -->
-                                        <h5 class="product-name text-center"><?php echo get_field('product_name')?get_field('product_name'):get_the_title();?></h5>
-                                    </a>
+                            <!-- Product Item -->
+                            <li class="product-item">
+                                <a href="<?php echo site_url('/products'); ?>">
+                                    <!-- Product image -->
+                                    <?php
+                                    $url_img = get_the_post_thumbnail_url($post->ID)? get_the_post_thumbnail_url($post->ID):get_theme_file_uri('assets/img/product1.png');
+                                    ?>
+                                    <img class="product-image" src="<?php echo $url_img; ?>">
 
-                                </li>
-                                <?php
-                            } endwhile;?>
+                                    <!-- Color Variation -->
+                                    <?php $list_color_pro = get_field('product_colors'); ?>
+                                    <?php if($list_color_pro): ?>
+                                        <ul class="color-variations horizontal-list text-right pr-15">
+                                            <?php foreach( $list_color_pro as $color_pro): ?>
+                                                <li class="color-variations__item <?php echo $color_pro; ?>"></li>
+                                            <?php endforeach; ?>
+                                        </ul><!-- Color Variation -->
+                                    <?php endif;?>
+
+                                    <!-- Product Name -->
+                                    <h5 class="product-name text-center">
+                                        <?php echo get_field('product_name') ? get_field('product_name') : get_the_title(); ?>
+                                    </h5>
+                                </a>
+
+                            </li>
+                        <?php endwhile; ?>
                     </ul>
-                <?php endif; wp_reset_postdata();?>
+                <?php endif; ?>
+                <?php wp_reset_postdata(); ?>
             </li>
+
+            <!-- Shop Searching Page -->
             <li class="nav-item <?php if(is_page('/shops')) echo 'active'; ?>">
                 <a class="nav-link" href="<?php echo site_url('/shops') ?>">Cửa hàng</a>
             </li>
+
+            <!-- About Us -->
             <li class="nav-item with-sub-menu">
                 <a class="nav-link">Về chúng tôi</a>
                 <i class="fa fa-plus nav-trigger active" aria-hidden="true"></i>
@@ -279,11 +275,16 @@
                     </li>
                 </ul>
             </li>
+
+            <!-- Support -->
             <li class="nav-item <?php if(is_page('support')) echo 'active'; ?>">
                 <a class="nav-link" href="<?php echo site_url('/support') ?>">Hỗ trợ</a>
             </li>
         </ul>
+        <!-- Main Mobile Dropdown Menu - END -->
+
     </div>
+    <!-- Mobile Drop Down Menu - END -->
 
 </header>
 <!-- === HEADER - END === -->
